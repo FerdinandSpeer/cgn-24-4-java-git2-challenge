90d5e5d262c432d0250e2ef8f8207424d84f704b
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-03 11:16:25 +0200

    Feature1 changed

--- src/main/java/de/speer/Main.java
+++ src/main/java/de/speer/Main.java
@@ -3,6 +3,6 @@ package de.speer;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Feature1");
+        System.out.println("Feature1 changed, commit and push");
     }
 }
