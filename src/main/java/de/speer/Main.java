d22df5a39edb380b3f4de3107bcb0f9954440baf
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-03 11:17:42 +0200

    Added changes to Main Branche

--- src/main/java/de/speer/Main.java
+++ src/main/java/de/speer/Main.java
@@ -2,6 +2,6 @@ package de.speer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!-> new changes for commit and push");
     }
 }
